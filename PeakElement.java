9eaa2b742b3ab3e3beabab0e0ba59fcfb942fc4a
AbhinayAmbati <abhinayambati4@gmail.com>
2025-09-07 12:19:37 +0530

    Solved Peak Element problem time : 12:19 date : 07-09-2025

---
+++ PeakElement.java
@@ -0,0 +1,15 @@
+class PeakElement {
+    public int peakElement(int[] arr) {
+        // code here
+        int n = arr.length;
+        if(n == 1 || arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+        int index = 0;
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i-1] < arr[i] && arr[i] > arr[i+1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
